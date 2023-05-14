d69b5f4cf25e9922bedc61d283035d43d9487f77
rahulbarik23 <rahul.barik1996@gmail.com>
2023-05-14 15:19:37 +0530

    checking test case

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,62 @@
 
-import React from "react";
+import React, {useState} from "react";
 import './../styles/App.css';
 
 const App = () => {
+
+   let [gender, setGender] = useState("")
+
+   
+
   return (
     <div>
-        {/* Do not remove the main div */}
+           <h2>Select Your gender:</h2>
+
+            <input 
+              type="radio"
+              name="gender"
+              value = "Male"
+              id="male"
+              onChange={(e) => setGender(e.target.value)}
+            />
+            <label forHTML="male">Male</label>
+            <input 
+              type="radio"
+              name="gender"
+              value = "Female"
+              id="female"
+              onChange={(e) => setGender(e.target.value)}
+            />
+            <label forHTML="female">Female</label>
+
+            {
+                gender == "Male" &&
+                <div>
+                        <h2>Select your shirt size:</h2>
+                        <select>
+                            <option value="select" disabled>Select size</option>
+                            <option value="small">Small</option>
+                            <option value="medium">Medium</option>
+                            <option value="large">Large</option>
+                        </select>
+
+                </div>
+            }
+
+          {
+                gender == "Female" &&
+                <div>
+                        <h2>Select your dress size:</h2>
+                        <select>
+                            <option value="select" disabled>Select size</option>
+                            <option value="two">2</option>
+                            <option value="four">4</option>
+                            <option value="six">6</option>
+                        </select>
+
+                </div>
+            }
+
     </div>
   )
 }
